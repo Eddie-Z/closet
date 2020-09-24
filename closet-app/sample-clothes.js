2dcd9ae9321d0574160cb7b703c6f512390ff7d3
Eddie-Z <35039865+Eddie-Z@users.noreply.github.com>
2020-09-23 21:24:09 -0700

    mock data

---
+++ closet-app/sample-clothes.js
@@ -0,0 +1,30 @@
+const clothes = {
+  clothes1: {
+    category: "top",
+    color: "white",
+    brand: "costco",
+    image: "https://picsum.photos/200/300/?blur",
+    washes: 2,
+    purchaseDate: "03/25/2015",
+    price: 2250,
+  },
+  clothes2: {
+    category: "bottoms",
+    color: "blue",
+    brand: "walmart",
+    image: "https://picsum.photos/200/300/?blur",
+    washes: 5,
+    purchaseDate: "03/25/2015",
+    price: 3050,
+  },
+  clothes3: {
+    category: "shoes",
+    color: "red",
+    brand: "superstore",
+    image: "https://picsum.photos/200/300/?blur",
+    washes: 2,
+    purchaseDate: "03/25/2015",
+    price: 2250,
+  },
+};
+export default clothes;

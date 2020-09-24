c5ebc8b2e4c2a2c25ba107e5e96a8ee3daf1e416
Eddie-Z <35039865+Eddie-Z@users.noreply.github.com>
2020-09-23 20:11:30 -0700

    added reach router

---
+++ closet-app/src/Closet.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Closet = () => {
+  return (
+    <div>
+      <h1>closet</h1>
+    </div>
+  );
+};
+
+export default Closet;
--- closet-app/src/index.js
+++ closet-app/src/index.js
@@ -1,14 +1,26 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import * as serviceWorker from "./serviceWorker";
+import { Router, Link } from "@reach/router";
+import Closet from "./Closet";
+
+let Home = () => <div>Home</div>;
+
+let Dashboard = () => <div>Dash</div>;
+let Generator = () => <div>Generate</div>;
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Router>
+      <App path="/" />
+      <Dashboard path="/dashboard" />
+      <Closet path="/closet" />
+      <Generator path="/generate" />
+    </Router>
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
 
 // If you want your app to work offline and load faster, you can change
